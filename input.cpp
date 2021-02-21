2d7bafbbd6a623b686c1e3f97414e663e2d30262
SylvesterStewart <sstewart2@pride.hofstra.org>
2021-02-21 07:30:52 +0000

    I answered questions on how the random funcition works and what will be outputted when using this function

--- input.cpp
+++ input.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream> 
+#include <climits>
 using namespace std;
 
 int main()
@@ -48,11 +49,12 @@ int main()
   
 
     output_file << "\nQ1: (cin only) What happens when you run the code above? Explain why" << endl;
-    string answer = ""; // enter your answer in between ""
+    string answer = "I entered 10 and the flag is false, but when I enter 4.5 it still sets the flag to false and it extracts the value 4, \n and it continues to loop without accepting any other values "; // enter your answer in between ""
     output_file << "Answer Q1: " << answer << endl;
 
     cout << "\nCode for Q2 and Q3" << endl;
-    /* //uncomment when you get to Q2 and Q3
+     //uncomment when you get to Q2 and Q3.5
+    
     for (int i = 0; i < array_values.size(); i++)
     {
         cout << "\tEnter the value " << array_values.at(i) << endl;
@@ -62,10 +64,9 @@ int main()
         cout << "\n\tCheck fail flag in cin " <<  cin.fail()<< endl;
 
         string s; 
-        getline(cin,s); 
+        getline(cin,s);
         cout << "\tCheck what is left unread in the cin buffer: Length =" << s.length() 
              << "\tString " << s << endl;
-
         if (cin.fail())
         {
             cin.clear(); // clears the flags   
@@ -75,20 +76,20 @@ int main()
         
         cout << "\tYou entered " << val << endl << endl;
     }  
-    */
+    
 
     output_file << "\nQ2: (cin + fail+ignore) What happens when you run the code above? Explain why." << endl;
-    answer = ""; // enter your answer in between ""
+    answer = "I entered 10 and it set the fail flag to false, then I entered 4.5 and it set the flag to false, but it shows that.5 was unread in the buffer \n, for abc and a it sets the fail flag to true and explains it is not an integer"; // enter your answer in between ""
     output_file << "Answer Q2: " << answer << endl;
-
+/*
     output_file << "\nQ3: Is the problem of reading an integer value solved? Explain your reasoning." << endl;
-    answer = ""; // enter your answer in between ""
+    answer = "No, because for 4.5 it set the flag to false and it extracted 4 without saying it is not an integer"; // enter your answer in between ""
     output_file << "Answer Q3: " << answer << endl;
 
     cout << "\nCode for Q4" << endl;
     // uncomment when you get to Q4
-   /*
-    for (int i = 0; i < array_values.size(); i++)
+  
+    /*for (int i = 0; i < array_values.size(); i++)
     {
         cout << "\tEnter the value " << array_values.at(i) << endl;
         cout << "\tEnter an int "; 
@@ -98,10 +99,10 @@ int main()
         val = stoi(s);  // stoi = converts a string into an integer 
                             // read more at http://www.cplusplus.com/reference/string/stoi/?kw=stoi
         cout << "\tYou entered " << val << endl << endl;
-    }  
-*/
+    } */ 
+
     output_file << "\nQ4: (getline only) What happens when you run the code above? Explain why." << endl;
-    answer = ""; // enter your answer in between ""
+    answer = "It allows you to enter the value for 10 and displays it, \n I then was able to enter 4.5 but it showed I entered 4, \n and then when I entered abc the program displayed an error message thrown by stoi function and then it terminates"; // enter your answer in between ""
     output_file << "Answer Q4: " << answer << endl;
 
     // Comment the for loop for Q4 above - leave the answer to Q4
@@ -123,8 +124,9 @@ int main()
         }
         cout << "\tYou entered " << val << endl << endl;
     }
+    cout << "\n\tCheck fail flag in cin " <<  cin.fail()<< endl;
     output_file << "\nQ5: (getline+stoi) What happens when you run the code above? Explain why." << endl;
-    answer = ""; // enter your answer in between ""
+    answer = "when you run the code you are able to enter 10 and it displays 10, then I entered 4.5, but it displays 4. Then when you enter abc and a then the program displays 4 and the error message from stoi. This is because the try and catch exception catches error and displays the message but it does not stop the program."; // enter your answer in between ""
     output_file << "Answer Q5: " << answer << endl;
 
     //6. Q6 Add code to enter integer values from the keyboard correctly = accept only 10, not 4.5, abc or a
@@ -132,7 +134,29 @@ int main()
     for (int i = 0; i < array_values.size(); i++)
     {
         cout << "\tEnter the value " << array_values.at(i) << endl;
-        cout << "\tEnter an int "; 
+        cout << "\tEnter an int ";
+        string s;
+        getline(cin, s);
+        bool floatingPoint = false;
+        for (int x = 0; x < s.length(); x++){
+            if ((s[x] == '.') || (s[x] == '/')){
+                floatingPoint = true;
+                cout << "\tIncorrect, you entered:" << s << endl;
+                break;
+            }
+        }
+        if (floatingPoint == false){
+            try{   // read more about exceptions at https://www.tutorialspoint.com/cplusplus/cpp_exceptions_handling.htm
+                val = stoi(s);
+                cout << "\tCorrect, you entered: " << s << endl;
+            
+            }
+            catch(const std::invalid_argument& ia)
+            {
+                cerr << "\tIncorrect, you entered: " << s << '\n';
+            }
+        }
+
 
         // add your code - it should ask you to type the value you see at line 134. 
         // it should display "Correct 10 " when you enter 10; 
@@ -141,7 +165,8 @@ int main()
         // YOUR CODE NEEDS TO DETECT that 4.5 is not an integer
     }
     output_file << "\nQ6: Explain your method to validate integer values entered" << endl;
-    answer = ""; // enter your answer in between ""
+    answer = "My method to validate integer values entered is to have the user input the value,\n then the input will be stored in a vairable s, after this\n the string s will be looped through to check if there is a decimal point or fraction present.\n if there is the code will print out it is incorrect and the value of s. If it does not find anything in the loop, the code will attempt to change the string to an integer and if it can it print it is correct and the value.\n if it cannot the code will print it is incorrect and the value you entered."; // enter your answer in between ""
     output_file << "Answer Q6: " << answer << endl;
     output_file.close();
+
 }
