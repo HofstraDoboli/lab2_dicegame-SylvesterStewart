638ce19015c306d4bd01fe0ce5cf2dc3c2bb8e9e
SylvesterStewart <sstewart2@pride.hofstra.org>
2021-02-24 03:36:12 +0000

    I revised my dice game to make my code more concise and easier to read, by moving the condition to play again all the way to the bottom and fixing indentation

--- play_dice.cpp
+++ play_dice.cpp
@@ -35,111 +35,75 @@ int main()
         cin >> userInput;
     }else if((userInput == "y") || (userInput == "Y")){
 
-        while ((userInput == "y") || (userInput == "Y")){
-            nrProblems++;
-            rollNr++;
-            num1 = ((rand()%6) + 1);
-            num2 = ((rand()%6) + 1);
-            cout << "your numbers are: " << num1 << " " <<num2 << endl;
-            sum1 = num1 + num2;
+            while ((userInput == "y") || (userInput == "Y")){
+                nrProblems++;
+                rollNr++;
+                num1 = ((rand()%6) + 1);
+                num2 = ((rand()%6) + 1);
+                cout << "your numbers are: " << num1 << " " <<num2 << endl;
+                sum1 = num1 + num2;
         
-        
-            if ((sum1 == 7) || (sum1 == 11)){
-                nrCProblems++;
-                cout << "Your sum is " << sum1 << " " << "YOU WIN" << endl;
-                cout << "do you want to play again? Y for yes or any other key for no" << endl;
-                cin >> userInput;
-                if ((userInput != "y") && (userInput != "Y")){
-                    break;
+                if ((sum1 == 7) || (sum1 == 11)){
+                    nrCProblems++;
+                    cout << "Your sum is " << sum1 << " " << "YOU WIN" << endl;
+                    cout << "do you want to play again? Y for yes or any other key for no" << endl;
+                    cin >> userInput;
+                   
                 }
-                else if(userInput == "y" || userInput == "Y"){
-                    continue;
+                else if ((sum1 == 2) || (sum1 == 3) || (sum1 == 12)){
+                    nrICProblems++;
+                    cout << "Your sum is " << sum1 << " " << " YOU LOSE" << endl;
+                    cout << "do you want to play again? Y for yes or any other key for no" << endl;
+                    cin >> userInput;
                 }
-            }
-            else if ((sum1 == 2) || (sum1 == 3) || (sum1 == 12)){
-                nrICProblems++;
-                cout << "Your sum is " << sum1 << " " << " YOU LOSE" << endl;
-                cout << "do you want to play again? Y for yes or any other key for no" << endl;
-                cin >> userInput;
-                if ((userInput != "y") && (userInput != "Y")){
-                    break;
-                }
-                else if(userInput == "y" || userInput == "Y"){
-                    continue;
-                }
-            }
-            else {
-                playersPoint = sum1;
-                cout << "Your points are now " << playersPoint << endl;
-                cout << "Please roll again: press r" << endl;
-                cin >> roll;
-                if ((roll != "r") && (roll != "R")) {
-                    cout << "incorrect input, please press r" << endl;
-                    cin >> roll;
-                }
-                if ((roll == "r") || (roll == "R")){
-                    rollNr++;
-                    num1 = ((rand()%6) + 1);
-                    num2 = ((rand()%6) + 1);
-                    cout << "your numbers are: " << num1 << " " <<num2 << endl;
-                    sum2 = num1 + num2;
-                    cout << "your sum is: " << sum2 << endl;
-                }
-                while ((sum2 != 7) && (sum2 != playersPoint)){
-                    cout << "Please roll again: press r" << endl;
-                    cin >> roll;
-                    if ((roll != "r") && (roll != "R")) {
-                        cout << "incorrect input, please press r" << endl;
+                else {
+                    playersPoint = sum1;
+                    cout << "Your points are now " << playersPoint << endl;
+                    do{
+                        cout << "Please roll again: press r" << endl;
                         cin >> roll;
+                        if ((roll != "r") && (roll != "R")) {
+                            cout << "incorrect input, please press r" << endl;
+                            cin >> roll;
+                        }
+                        if ((roll == "r") || (roll == "R")){
+                            rollNr++;
+                            num1 = ((rand()%6) + 1);
+                            num2 = ((rand()%6) + 1);
+                            cout << "your numbers are: " << num1 << " " <<num2 << endl;
+                            sum2 = num1 + num2;
+                            cout << "Your original points is " << playersPoint << endl;
+                            cout << "your sum is: " << sum2 << endl;
+                        }
+                    }while ((sum2 != 7) && (sum2 != playersPoint));
+            
+                    if (sum2 == 7){
+                        nrICProblems++;
+                        cout << "your points were: " << sum2 << " YOU LOSE" << endl;
+                        cout << "do you want to play again? Y for yes or any other key for no" << endl;
+                        cin >> userInput;
                     }
-                    if ((roll == "r") || (roll == "R")){
-                        rollNr++;
-                        num1 = ((rand()%6) + 1);
-                        num2 = ((rand()%6) + 1);
-                        cout << "your numbers are: " << num1 << " " <<num2 << endl;
-                        sum2 = num1 + num2;
-                        cout << "Your original points is " << playersPoint << endl;
-                        cout << "your sum is: " << sum2 << endl;
+
+                    if (playersPoint == sum2){
+                        nrCProblems++;
+                        cout << "Your points were: " << sum2 << " YOU WIN" << endl;
+                        cout << "do you want to play again? Y for yes or any other key for no" << endl;
+                        cin >> userInput;
                     }
                 }
-            }
-            if (sum2 == 7){
-                nrICProblems++;
-                cout << "your points were: " << sum2 << " YOU LOSE" << endl;
-                cout << "do you want to play again? Y for yes or any other key for no" << endl;
-                cin >> userInput;
                 if ((userInput != "y") && (userInput != "Y")){
                     break;
                 }
                 else if(userInput == "y" || userInput == "Y"){
                     continue;
                 }
+
+
             }
-
-            if (playersPoint == sum2){
-                nrCProblems++;
-                cout << "Your points were: " << sum2 << " YOU WIN" << endl;
-                cout << "do you want to play again? Y for yes or any other key for no" << endl;
-                cin >> userInput;
-                if ((userInput != "y") && (userInput != "Y")){
-                    break;
-                }
-                else if(userInput == "y" || userInput == "Y"){
-                    continue;
-                }
-            }
-
-
+        }else{
+            cout << "Game over" << endl;
         }
-    }else{
-        cout << "Game over" << endl;
-    }
-    if (userInput != "y" && userInput != "Y"){
         cout << "You played " << nrProblems << " games," << "You won " << nrCProblems << " games," << "You lost " << nrICProblems << " games!" << " You rolled " << rollNr << " times!" << endl;
-
-    }
-     
-
 }
 
 
